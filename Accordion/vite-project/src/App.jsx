8d1287fae760309e1eb3eddb804f77439b488c67
bbscoding <batuhan5534@gmail.com>
2024-04-04 00:39:39 +0300

    accordion + randomColorGenerator

---
+++ Accordion/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Accordion from './components/accordion'
+import RandomColor from './components/random-color'
+
+function App() {
+
+  return (
+    <>
+      {/* Accordion Component */}
+      {/* <Accordion /> */}
+      {/* Random Color Component */}
+      <RandomColor />
+    </>
+  )
+}
+
+export default App
---
+++ Accordion/vite-project/src/components/accordion/data.js
@@ -0,0 +1,25 @@
+const data = [
+    {
+      id: 1,
+      question: "En sevdiğiniz renk nedir?",
+      answer: "Mavi"
+    },
+    {
+      id: 2,
+      question: "En sevdiğiniz meyve hangisidir?",
+      answer: "Elma"
+    },
+    {
+      id: 3,
+      question: "Hangi mevsimi daha çok seversiniz?",
+      answer: "İlkbahar"
+    },
+    {
+      id: 4,
+      question: "Hangi tür müzikleri seversiniz?",
+      answer: "Rock"
+    }
+  ];
+
+  export default data;
+  
