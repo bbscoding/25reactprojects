1c9cd92ad61cff0278cd1c7de4a492ec8d9b019a
bbscoding <batuhan5534@gmail.com>
2024-04-04 23:46:42 +0300

    third project

--- Accordion/vite-project/src/App.jsx
+++ Accordion/vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Accordion from './components/accordion'
 import RandomColor from './components/random-color'
+import StarRating from './components/star-rating'
 
 function App() {
 
@@ -9,7 +10,9 @@ function App() {
       {/* Accordion Component */}
       {/* <Accordion /> */}
       {/* Random Color Component */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+      {/* Star Rating Component */}
+      <StarRating noOfStars={10} />
     </>
   )
 }
