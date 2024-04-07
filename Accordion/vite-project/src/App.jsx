310304c5c42c2892c2a44776151c051af007ec7d
bbscoding <batuhan5534@gmail.com>
2024-04-07 14:09:43 +0300

    4th and 5th project

--- Accordion/vite-project/src/App.jsx
+++ Accordion/vite-project/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import Accordion from './components/accordion'
+import ImageSlider from './components/image-slider'
+import LoadMoreData from './components/load-more-data'
 import RandomColor from './components/random-color'
 import StarRating from './components/star-rating'
 
@@ -9,10 +11,18 @@ function App() {
     <>
       {/* Accordion Component */}
       {/* <Accordion /> */}
+
       {/* Random Color Component */}
       {/* <RandomColor /> */}
+
       {/* Star Rating Component */}
-      <StarRating noOfStars={10} />
+      {/* <StarRating noOfStars={10} /> */}
+
+      {/* Image Slider Component */}
+      {/* <ImageSlider url={`https://picsum.photos/v2/list`} page={"1"} limit={"10"} /> */}
+
+      {/* Load More Products Component */}
+      <LoadMoreData />
     </>
   )
 }
